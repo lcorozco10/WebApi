27110d86b21d8bf271431081326744b5977c73c8
Luis <lcorozco10@gmail.com>
2016-01-27 10:03:21 -0600

    Initial Commit

---
+++ Api.Rest.Secure/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using Api.Rest.Secure.App_Start;
+using Api.Rest.Secure.Providers;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(Api.Rest.Secure.Startup))]
+namespace Api.Rest.Secure
+{
+    public class Startup
+    {
+
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            ConfigureOAuth(app);
+
+            WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+           
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider(),
+              
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());            
+
+        }
+    }
+}
